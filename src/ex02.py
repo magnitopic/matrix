1ea67e6cfa1a9dd3f9b8d330cdf4f4bb1e2da8a2
magnitopic <magnitopic@gmail.com>
2025-11-30 20:19:51 +0100

    feat: ex02

---
+++ src/ex02.py
@@ -0,0 +1,32 @@
+from typing import TypeVar
+from Vector import Vector
+
+T = TypeVar('T', Vector, complex)
+
+
+def linear_interpolation(u: T, v: T, t: int | float) -> T:
+    if not isinstance(t, (int, float)):
+        raise TypeError("t must be a number")
+    if not 0 <= t <= 1:
+        raise ValueError("t must be in the range [0, 1]")
+
+    return (1 - t) * u + t * v
+
+
+def main():
+    print("Linear Interpolation of Vectors")
+    v1 = Vector([2.0, 1.0])
+    v2 = Vector([4.0, 2.0])
+
+    print(f"lerp({v1}, {v2}, 0.0) = {linear_interpolation(v1, v2, 0.0)}")
+    print(f"lerp({v1}, {v2}, 0.3) = {linear_interpolation(v1, v2, 0.3)}")
+    print(f"lerp({v1}, {v2}, 0.5) = {linear_interpolation(v1, v2, 0.5)}")
+    print(f"lerp({v1}, {v2}, 1.0) = {linear_interpolation(v1, v2, 1.0)}")
+
+    print("\n", "-" * 40, "\n")
+
+    print("Linear Interpolation of Matrices")
+
+
+if __name__ == "__main__":
+    main()
