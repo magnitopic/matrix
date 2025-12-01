cc3a546d2ac3d3f2d9e9b486c54d8e9bbc7ffa8d
magnitopic <magnitopic@gmail.com>
2025-12-01 13:20:41 +0100

    feat: ex02

--- src/ex02.py
+++ src/ex02.py
@@ -6,14 +6,15 @@
 #    By: alaparic <alaparic@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/11/30 20:56:08 by alaparic          #+#    #+#              #
-#    Updated: 2025/11/30 20:56:09 by alaparic         ###   ########.fr        #
+#    Updated: 2025/12/01 13:19:50 by alaparic         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 from typing import TypeVar
 from Vector import Vector
+from Matrix import Matrix
 
-T = TypeVar('T', Vector, complex)
+T = TypeVar('T', Vector, Matrix, complex)
 
 
 def linear_interpolation(u: T, v: T, t: int | float) -> T:
@@ -31,13 +32,19 @@ def main():
     v2 = Vector([4.0, 2.0])
 
     print(f"lerp({v1}, {v2}, 0.0) = {linear_interpolation(v1, v2, 0.0)}")
-    print(f"lerp({v1}, {v2}, 0.3) = {linear_interpolation(v1, v2, 0.3)}")
     print(f"lerp({v1}, {v2}, 0.5) = {linear_interpolation(v1, v2, 0.5)}")
     print(f"lerp({v1}, {v2}, 1.0) = {linear_interpolation(v1, v2, 1.0)}")
 
     print("\n", "-" * 40, "\n")
 
     print("Linear Interpolation of Matrices")
+    m1 = Matrix([[1.0, 2.0], [3.0, 4.0]])
+    m2 = Matrix([[5.0, 6.0], [7.0, 8.0]])
+    print(f"lerp(\n{m1},\n{m2}, 0.0) \n=\n{linear_interpolation(m1, m2, 0.0)}")
+    print("-" * 10)
+    print(f"lerp(\n{m1},\n{m2}, 0.5) \n=\n{linear_interpolation(m1, m2, 0.5)}")
+    print("-" * 10)
+    print(f"lerp(\n{m1},\n{m2}, 1.0) \n=\n{linear_interpolation(m1, m2, 1.0)}")
 
 
 if __name__ == "__main__":
