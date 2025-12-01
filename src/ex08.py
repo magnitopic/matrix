53c66f98de046d75c7567d634f5500b0c42b7423
magnitopic <magnitopic@gmail.com>
2025-12-01 18:15:14 +0100

    feat: ex08

---
+++ src/ex08.py
@@ -0,0 +1,63 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    ex08.py                                            :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: alaparic <alaparic@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2025/12/01 17:59:14 by alaparic          #+#    #+#              #
+#    Updated: 2025/12/01 18:14:33 by alaparic         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+from Vector import Vector
+
+from Matrix import Matrix
+
+
+def trace(matrix: Matrix) -> float:
+    if matrix.shape()[0] != matrix.shape()[1]:
+        raise ValueError("Trace is only defined for square matrices.")
+
+    return sum(matrix.data[i][i] for i in range(matrix.shape()[0]))
+
+
+def main():
+    mat = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ]
+    matrix = Matrix(mat)
+    print("Matrix:")
+    for row in matrix.data:
+        print(row)
+    print("Trace of the matrix:", trace(matrix))
+
+    mat = [
+        [3, 4, 1],
+        [5, 2, 8],
+        [6, 9, 7]
+    ]
+    matrix = Matrix(mat)
+    print("\nMatrix:")
+    for row in matrix.data:
+        print(row)
+    print("Trace of the matrix:", trace(matrix))
+
+    mat = [
+        [1, 2, 3],
+        [4, 5, 6]
+    ]
+    matrix = Matrix(mat)
+    print("\nMatrix:")
+    for row in matrix.data:
+        print(row)
+    try:
+        print("Trace of the matrix:", trace(matrix))
+    except ValueError as e:
+        print("Error:", e)
+
+
+if __name__ == "__main__":
+    main()
